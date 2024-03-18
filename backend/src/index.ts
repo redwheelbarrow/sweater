60e92dd7e92b93c7a75b76d754e573d27e3bff83
Caelin Childs <m_chi919@msn.com>
2024-03-18 15:12:10 -0600

    Initial

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript Node.js server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
